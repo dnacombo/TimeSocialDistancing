91eac35a7eeb2cb2152e611da500f38fc1660a19
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2020-06-08 00:29:45 +0200

    Big update

--- helpers.R
+++ helpers.R
@@ -1,9 +1,32 @@
+library(tidyverse)
+
+if (file.exists(f <- file.path(params$rootdir,'NodeKeys.csv')))   {
+  allnodes <- read_csv(f,col_types = cols())
+} else {
+  allnodes <- gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1Mwy2aGCJ6vSpp4a32NOs83e2H73MQRFUOL_193yb8sQ/edit#gid=0') %>%
+    # rename columns of interest with simple names
+    rename(order = 1,
+           UniqueName = 4) %>%
+    # discard the comment line at the beginning of the table
+    filter(order != 'Comment') %>%
+    write_csv(f)
+}
+
+
+if (file.exists(f <- file.path(params$rootdir,'ExperimentIDs.csv')))   {
+  experimentIDs <- read_csv(f,col_types = cols())
+} else {
+  experimentIDs <- gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1p6_WHQXNGFw2EJGny1jb5qivMy2pJ_VRRYoDGRLxgbY/edit#gid=0') %>%
+    write_csv(f)
+}
+
+ExperimentName <- dplyr::filter(experimentIDs,`Experiment ID` == params$experimentID)$ExperimentName
+
 
 Q_cols <- cols(`Participant Private ID` = col_factor())
 
 
 Q_read <- function(datafiles) {
-  
   orig <- tibble()
   for (f in datafiles) {
     orig <- read_csv(f,col_types = cols()) %>%
@@ -14,11 +37,13 @@ Q_read <- function(datafiles) {
   orig %>%
     mutate(`Participant Private ID` = as.factor(`Participant Private ID`))
 }
+
+
 Q_Nsuj <- function(orig){
   
   N <- orig %>% filter(`Question Key` %in% 'END QUESTIONNAIRE') %>%
     group_by(File) %>%
-    dplyr::summarize(N = n())
+    summarize(N = n())
 }
 
 Q_Complete <- function(orig) {
@@ -27,9 +52,45 @@ Q_Complete <- function(orig) {
     mutate(Response = as.numeric(Response)/1000)
   p <- ggplot(d,aes(x = `Participant Private ID`, y=Response,fill= `Participant Private ID`)) +
     geom_col(show.legend = F) +
+    scale_y_log10() +
     theme(axis.text.x = element_text(angle = 30,hjust = 1)) +
     facet_wrap(~File) +
-    ylab('Time to complete (s)')
+    ylab('Time to complete (s, log scale)')
   print(p)
   
 }
+
+
+T_read <- function(datafiles) {
+  orig <- tibble()
+  for (f in datafiles) {
+    orig <- read_csv(f,col_types = cols()) %>%
+      mutate(File = basename(f))%>%
+      bind_rows(orig)
+  }
+  
+  orig %>%
+    mutate(`Participant Private ID` = as.factor(`Participant Private ID`))
+}
+
+T_Nsuj <- function(orig){
+  
+  N <- orig %>% filter(`Trial Number` %in% 'END TASK') %>%
+    group_by(File) %>%
+    summarize(N = n())
+}
+
+T_Complete <- function(orig) {
+  d <- orig %>% filter(`Trial Number` %in% 'END TASK') %>%
+    group_by(File) %>%
+    mutate(`Reaction Time` = as.numeric(`Reaction Time`)/1000)
+  p <- ggplot(d,aes(x = `Participant Private ID`, y=`Reaction Time`,fill= `Participant Private ID`)) +
+    geom_col(show.legend = F) +
+    theme(axis.text.x = element_text(angle = 30,hjust = 1)) +
+    facet_wrap(~File) +
+    ylab('Time to complete (s, log scale)') +
+    scale_y_log10()
+  print(p)
+  
+}
+
