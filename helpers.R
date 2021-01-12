9589c8f6843f4b9f967604b13724cb7fba78639a
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2021-01-12 13:48:47 +0100

    update helpers functions: gimmedata accepts file argument T_ and Q_ functions adapted to work with gimmedata

--- helpers.R
+++ helpers.R
@@ -1,5 +1,48 @@
 library(tidyverse)
 
+if (!exists('online')) {online <- F}
+
+
+gimmedata <- function(DataDir = getwd(), ExperimentID = '[0-9]{5}', ExperimentName = '.*', UniqueName = '.*', Session = '.*', Run = '.*', file = '',  clean = T, verbose = T) {
+  
+  if (file != '') {
+    if (! file.exists(file)) {stop('File provided does not exist')}
+    fs <- file
+  } else {
+    p <- paste0('data_exp_([0-9]{5}-)*', ExperimentID, '(-[0-9]{5})*', '_', ExperimentName, '_Session', Session)
+    d <- list.files(path = DataDir, pattern = p,full.names = T)
+    if (verbose) {
+      cat(paste0('Loading data from ', str_replace(d,DataDir,'')),sep = '\n')
+    }
+    
+    p <- paste0('S',Session,'_', UniqueName, '(_r',Run,')?.csv')
+    fs <- list.files(path = file.path(d),pattern = p,full.names = T)
+  }
+  if (length(fs) == 0) { stop(paste0('Could not find data (', p, ')'))}
+  
+  d <- tibble()
+  for (f in fs) {
+    FF <- str_match_all(basename(f),'(S[^_]*)_([^_]*)_?r?([^\\.]*)?.csv')
+    Session <- FF[[1]][2]
+    UniqueName <- FF[[1]][3]
+    Run <- FF[[1]][4]
+    
+    if (verbose) {cat(paste0('Loading ',str_replace(f,DataDir,'')),sep = '\n')}
+    
+    tmp <- read_csv(f,col_types = cols(.default = col_character())) %>%
+      mutate(Session = as.character(Session),
+             UniqueName = as.character(UniqueName),
+             Run = as.character(Run))
+    if (clean){
+      tmp <- tmp %>% select(-starts_with('order-'),-starts_with('checkpoint-'),-starts_with('branch-'),-matches('Schedule ID'),-starts_with('Participant'),`Participant Private ID`)
+    }
+    d <- bind_rows(d,tmp)
+  }
+  d %>% select(Session,UniqueName,Run,matches('PID'),everything()) %>%
+    mutate(`UTC Date` = lubridate::dmy_hms(`UTC Date`),
+           `Local Date` = lubridate::dmy_hms(`Local Date`))
+}
+
 source_rmd <- function(file, local = FALSE, ...){
   options(knitr.duplicate.label = 'allow')
   
@@ -12,75 +55,6 @@ source_rmd <- function(file, local = FALSE, ...){
 }
 
 
-gimmedata <- function(DataDir = getwd(), ExperimentID = '[0-9]{5}', ExperimentName = '.*', UniqueName = '.*', Session = '.*', Run = '.*', clean = T, verbose = T) {
-  
-  p <- paste0('data_exp_([0-9]{5}-)*', ExperimentID, '(-[0-9]{5})*', '_', ExperimentName, '_Session', Session)
-  d <- list.files(path = DataDir, pattern = p,full.names = T)
-  if (verbose) {
-    cat(paste0('Loading data from ', str_replace(d,DataDir,'')),sep = '\n')
-  }
-  
-  p <- paste0('S',Session,'_', UniqueName, '_r',Run,'.csv')
-  fs <- list.files(path = file.path(d),pattern = p,full.names = T)
-  if (length(fs) == 0) { stop(paste0('Could not find data (', p, ')'))}
-  
-  d <- tibble()
-  for (f in fs) {
-    FF <- str_match_all(basename(f),'(S[^_]*)_([^_]*)_r([^\\.]*)')
-    Session <- FF[[1]][2]
-    UniqueName <- FF[[1]][3]
-    Run <- FF[[1]][4]
-    
-    if (verbose) {cat(paste0('Loading ',str_replace(f,DataDir,'')),sep = '\n')}
-    
-    d <- read_csv(f,col_types = cols(.default = col_character())) %>%
-      mutate(Session = as.character(Session),
-             UniqueName = as.character(UniqueName),
-             Run = as.character(Run)) %>%
-      bind_rows(d,.)
-  }
-  if (clean){
-    d <- d %>% select(-starts_with('order-'),-starts_with('checkpoint-'),-starts_with('branch-'),-`Schedule ID`,-starts_with('Participant'),`Participant Private ID`)
-  }
-  d %>% select(Session,UniqueName,Run,matches('PID'),everything()) %>%
-    mutate(`UTC Date` = lubridate::dmy_hms(`UTC Date`),
-           `Local Date` = lubridate::dmy_hms(`Local Date`))
-}
-
-f <- file.path(params$rootdir,'NodeKeys.csv')
-  allnodes.S1 <- gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1Mwy2aGCJ6vSpp4a32NOs83e2H73MQRFUOL_193yb8sQ/edit#gid=0') %>%
-    filter(prefix != 'Comment')
-  
-  allnodes.S2 <- gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1Mwy2aGCJ6vSpp4a32NOs83e2H73MQRFUOL_193yb8sQ/edit#gid=1606433485') %>%
-    filter(prefix != 'Comment')
-  
-  allnodes.S3 <- gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1Mwy2aGCJ6vSpp4a32NOs83e2H73MQRFUOL_193yb8sQ/edit#gid=1814296211') %>%
-    filter(prefix != 'Comment')
-
-  (allnodes <- bind_rows(allnodes.S1,
-                        allnodes.S2,
-                        allnodes.S3,.id = "Session")%>%
-    mutate_all(.funs = ~ na_if(.,'N/A'))) %>%
-    write_csv(f)
-rm(allnodes.S1,allnodes.S2,allnodes.S3)
-
-f <- file.path(params$rootdir,'ExperimentIDs.csv')
-  ExperimentIDs <- gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1p6_WHQXNGFw2EJGny1jb5qivMy2pJ_VRRYoDGRLxgbY/edit#gid=0') %>%
-    pivot_longer(cols=starts_with('Session'),names_to = 'Session', values_to = 'Experiment ID',names_prefix = 'Session',values_drop_na = T) %>%
-    write_csv(f)
-
-if (!is_null(params$ExperimentID)) {
-  ExperimentID <- params$ExperimentID
-  ExperimentName <- dplyr::filter(ExperimentIDs,`Experiment ID` %in% params$ExperimentID)$ExperimentName
-}else{
-  if (!is_null(params$ExperimentName)) {
-    ExperimentName <- params$ExperimentName
-    ExperimentID <- dplyr::filter(ExperimentIDs,ExperimentName == params$ExperimentName)$`Experiment ID`
-  }else {stop('Give either ExperimentName or Experiment ID')}
-  
-}
-Session <- dplyr::filter(ExperimentIDs,`Experiment ID` %in% ExperimentID)$Session
-
 Q_cols <- cols(`Participant Private ID` = col_factor())
 
 
@@ -88,7 +62,6 @@ Q_read <- function(datafiles) {
   orig <- tibble()
   for (f in datafiles) {
     orig <- read_csv(f,col_types = cols(.default = col_character())) %>%
-      mutate(File = basename(f))%>%
       bind_rows(orig)
   }
   
@@ -99,21 +72,21 @@ Q_read <- function(datafiles) {
 
 Q_Nsuj <- function(orig){
   N <- orig %>% filter(`Question Key` %in% 'END QUESTIONNAIRE') %>%
-    select(`Participant Private ID`,File,`Experiment ID`) %>%
-    group_by(File,`Experiment ID`) %>%
+    select(`Participant Private ID`,Session,Run,`Experiment ID`,UniqueName) %>%
+    group_by(Session,Run,`Experiment ID`,UniqueName) %>%
     distinct() %>%
     summarize(N = n())
 }
 
 Q_Complete <- function(orig) {
   d <- orig %>% filter(`Question Key` %in% 'END QUESTIONNAIRE') %>%
-    group_by(File) %>%
+    group_by(Session,Run,`Experiment ID`) %>%
     mutate(Response = as.numeric(Response)/1000)
   p <- ggplot(d,aes(x = `Participant Private ID`, y=Response,fill= `Participant Private ID`)) +
     geom_col(show.legend = F) +
     scale_y_log10() +
     theme(axis.text.x = element_text(angle = 30,hjust = 1)) +
-    facet_wrap(~File) +
+    facet_grid(Session~Run) +
     ylab('Time to complete (s, log scale)')
   print(p)
   
@@ -124,7 +97,6 @@ T_read <- function(datafiles) {
   orig <- tibble()
   for (f in datafiles) {
     orig <- read_csv(f,col_types = cols(.default = col_character())) %>%
-      mutate(File = basename(f))%>%
       bind_rows(orig)
   }
   
@@ -134,23 +106,75 @@ T_read <- function(datafiles) {
 
 T_Nsuj <- function(orig){
   N <- orig %>% filter(`Trial Number` %in% 'END TASK') %>%
-    select(`Participant Private ID`,File,`Experiment ID`) %>%
-    group_by(File,`Experiment ID`) %>%
+    select(`Participant Private ID`,Session,Run,`Experiment ID`,UniqueName) %>%
+    group_by(Session,Run,`Experiment ID`,UniqueName) %>%
     distinct() %>%
     summarize(N = n())
 }
 
 T_Complete <- function(orig) {
   d <- orig %>% filter(`Trial Number` %in% 'END TASK') %>%
-    group_by(File) %>%
-    mutate(`Reaction Time` = as.numeric(`Reaction Time`)/1000)
-  p <- ggplot(d,aes(x = `Participant Private ID`, y=`Reaction Time`,fill= `Participant Private ID`)) +
+    group_by(Session,Run, UniqueName,`Experiment ID`,`Repeat Key`) %>%
+    mutate(Duration = as.numeric(`Reaction Time`)/1000)
+  
+  p <- ggplot(d,aes(x = `Participant Private ID`, y = Duration,fill= `Participant Private ID`)) +
     geom_col(show.legend = F) +
     theme(axis.text.x = element_text(angle = 30,hjust = 1)) +
-    facet_wrap(~File) +
+    facet_grid(Session~Run) +
     ylab('Time to complete (s, log scale)') +
     scale_y_log10()
-  print(d %>% summarize(n=n()))
+  
+  print(d %>% group_by(Session,Run, UniqueName,`Experiment ID`) %>% summarize(n=n()))
   print(p)
   
+  return(d %>% select(Session, Run, UniqueName, `Participant Private ID`,`Repeat Key`, Duration))
+  
 }
+
+f <- file.path(params$rootdir,'NodeKeys.csv')
+
+if (online) {
+  allnodes.S1 <- gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1Mwy2aGCJ6vSpp4a32NOs83e2H73MQRFUOL_193yb8sQ/edit#gid=0') %>%
+    filter(prefix != 'Comment')
+  
+  allnodes.S2 <- gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1Mwy2aGCJ6vSpp4a32NOs83e2H73MQRFUOL_193yb8sQ/edit#gid=1606433485') %>%
+    filter(prefix != 'Comment')
+  
+  allnodes.S3 <- gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1Mwy2aGCJ6vSpp4a32NOs83e2H73MQRFUOL_193yb8sQ/edit#gid=1814296211') %>%
+    filter(prefix != 'Comment')
+  
+  (allnodes <- bind_rows(allnodes.S1,
+                         allnodes.S2,
+                         allnodes.S3,.id = "Session")%>%
+      mutate_all(.funs = ~ na_if(.,'N/A'))) %>%
+    write_csv(f)
+  
+  rm(allnodes.S1,allnodes.S2,allnodes.S3)
+} else {
+  allnodes <- read_csv(f, col_types = cols())
+}
+
+f <- file.path(params$rootdir,'ExperimentIDs.csv')
+
+if (online) {
+  ExperimentIDs <- gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1p6_WHQXNGFw2EJGny1jb5qivMy2pJ_VRRYoDGRLxgbY/edit#gid=0') %>%
+    pivot_longer(cols=starts_with('Session'),names_to = 'Session', values_to = 'Experiment ID',names_prefix = 'Session',values_drop_na = T) %>%
+    write_csv(f)
+} else {
+  ExperimentIDs <- read_csv(f,col_types = cols())
+}
+
+if (!is_null(params$ExperimentID)) {
+  ExperimentID <- params$ExperimentID
+  ExperimentName <- dplyr::filter(ExperimentIDs,`Experiment ID` %in% params$ExperimentID)$ExperimentName
+}else{
+  if (!is_null(params$ExperimentName)) {
+    ExperimentName <- params$ExperimentName
+    ExperimentID <- dplyr::filter(ExperimentIDs,ExperimentName == params$ExperimentName)$`Experiment ID`
+  }else {
+    ExperimentID <- ExperimentIDs$`Experiment ID`
+    ExperimentName <- dplyr::filter(ExperimentIDs,`Experiment ID` %in% ExperimentID)$ExperimentName
+  }
+}
+
+Session <- dplyr::filter(ExperimentIDs,`Experiment ID` %in% ExperimentID)$Session
