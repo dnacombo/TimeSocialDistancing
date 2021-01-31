cfe6f01bada101202b163eafcd58df19f192adb6
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2021-01-31 11:25:26 +0100

    Change dataunzip to MainDataUnzip and DataUnzip

---
+++ MainDataUnzip.R
@@ -0,0 +1,170 @@
+
+source('helpers.R')
+
+################################ FR #######################################
+Country = 'FR'
+
+l <- paramsMatch(Country=Country,experimentIDs = ExperimentIDs)
+l$rootdir <- params$rootdir
+  
+rmarkdown::render(
+  'DataNuUnzip.Rmd',
+  params = l,
+)
+
+rmarkdown::render(
+  paste0('DataPIDMatch_', Country[1], '.Rmd'),
+  output_file = paste0('DataPIDMatch_', Country[1]),
+  params = l,
+)
+
+rmarkdown::render(
+  paste0('DateLayout.Rmd'),
+  output_file = paste0('DateLayout_', Country[1]),
+  params = l,
+)
+
+############################# TR #############################################
+Country = 'TR'
+
+l <- paramsMatch(Country=Country,experimentIDs = ExperimentIDs)
+l$rootdir <- params$rootdir
+  
+rmarkdown::render(
+  'DataNuUnzip.Rmd',
+  params = l,
+)
+
+rmarkdown::render(
+  paste0('DataPIDMatch_', Country[1], '.Rmd'),
+  output_file = paste0('DataPIDMatch_', Country[1]),
+  params = l,
+)
+
+
+rmarkdown::render(
+  paste0('DateLayout.Rmd'),
+  output_file = paste0('DateLayout_', Country[1]),
+  params = l,
+)
+
+########################### IN ##################################################
+Country = 'IN'
+
+l <- paramsMatch(Country=Country,experimentIDs = ExperimentIDs)
+l$rootdir <- params$rootdir
+  
+rmarkdown::render(
+  'DataNuUnzip.Rmd',
+  params = l,
+)
+
+rmarkdown::render(
+  paste0('DataPIDMatch_', Country[1], '.Rmd'),
+  output_file = paste0('DataPIDMatch_', Country[1]),
+  params = l,
+)
+
+rmarkdown::render(
+  paste0('DateLayout.Rmd'),
+  output_file = paste0('DateLayout_', Country[1]),
+  params = l,
+)
+
+
+############################# GR ################################################
+Country = 'GR'
+
+l <- paramsMatch(Country=Country,experimentIDs = ExperimentIDs)
+l$rootdir <- params$rootdir
+  
+rmarkdown::render(
+  'DataNuUnzip.Rmd',
+  params = l,
+)
+
+rmarkdown::render(
+  paste0('DataPIDMatch_', Country[1], '.Rmd'),
+  output_file = paste0('DataPIDMatch_', Country[1]),
+  params = l,
+)
+
+rmarkdown::render(
+  paste0('DateLayout.Rmd'),
+  output_file = paste0('DateLayout_', Country[1]),
+  params = l,
+)
+
+
+############################# JP ###############################################
+Country = 'JP'
+
+l <- paramsMatch(Country=Country,experimentIDs = ExperimentIDs)
+l$rootdir <- params$rootdir
+  
+rmarkdown::render(
+  'DataNuUnzip.Rmd',
+  params = l,
+)
+
+rmarkdown::render(
+  paste0('DataPIDMatch_', Country[1], '.Rmd'),
+  output_file = paste0('DataPIDMatch_', Country[1]),
+  params = l,
+)
+
+rmarkdown::render(
+  paste0('DateLayout.Rmd'),
+  output_file = paste0('DateLayout_', Country[1]),
+  params = l,
+)
+
+############################# CO ###############################################
+Country = 'CO'
+
+l <- paramsMatch(Country=Country,experimentIDs = ExperimentIDs)
+l$rootdir <- params$rootdir
+  
+rmarkdown::render(
+  'DataNuUnzip.Rmd',
+  params = l,
+)
+
+rmarkdown::render(
+  paste0('DateLayout.Rmd'),
+  output_file = paste0('DateLayout_', Country[1]),
+  params = l,
+)
+
+########################### CA-FR ##############################################
+Country = 'CA'
+
+l <- paramsMatch(Country=Country,experimentIDs = ExperimentIDs)
+l$rootdir <- params$rootdir
+  
+rmarkdown::render(
+  'DataNuUnzip.Rmd',
+  params = l,
+)
+
+rmarkdown::render(
+  paste0('DateLayout.Rmd'),
+  output_file = paste0('DateLayout_', Country[1]),
+  params = l,
+)
+
+########################### AR ################################################
+Country = 'AR'
+
+l <- paramsMatch(Country=Country,experimentIDs = ExperimentIDs)
+l$rootdir <- params$rootdir
+  
+rmarkdown::render(
+  'DataNuUnzip.Rmd',
+  params = l,
+)
+rmarkdown::render(
+  paste0('DateLayout.Rmd'),
+  output_file = paste0('DateLayout_', Country[1]),
+  params = l,
+)
