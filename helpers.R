c24959e5a9a883dc94cce2253a20f416d3525809
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2021-01-31 23:12:33 +0100

    source gimmedata in helpers.R

--- helpers.R
+++ helpers.R
@@ -14,45 +14,7 @@ if (!exists('params')) {
 }
 
 
-gimmedata <- function(DataDir = getwd(), ExperimentID = '[0-9]{5}', ExperimentName = '.*', UniqueName = '.*', Session = '.*', Run = '.*', file = '',  clean = T, verbose = T) {
-  
-  if (file != '') {
-    if (! file.exists(file)) {stop('File provided does not exist')}
-    fs <- file
-  } else {
-    p <- paste0('data_exp_([0-9]{5}-)*(', ExperimentID, ')(-[0-9]{5})*', '_(', ExperimentName, ')(_Session', Session, ')?')
-    d <- list.files(path = DataDir, pattern = p,full.names = T)
-    if (verbose) {
-      cat(paste0('Loading data from ', str_replace(d,DataDir,'')),sep = '\n')
-    }
-    
-    p <- paste0('S',Session,'_', UniqueName, '(_r',Run,')?.csv')
-    fs <- list.files(path = file.path(d),pattern = p,full.names = T)
-  }
-  if (length(fs) == 0) { stop(paste0('Could not find data (', p, ')'))}
-  
-  d <- tibble()
-  for (f in fs) {
-    FF <- str_match_all(basename(f),'(S[^_]*)_([^_]*)_?r?([^\\.]*)?.csv')
-    Session <- FF[[1]][2]
-    UniqueName <- FF[[1]][3]
-    Run <- FF[[1]][4]
-    
-    if (verbose) {cat(paste0('Loading ',str_replace(f,DataDir,'')),sep = '\n')}
-    
-    tmp <- read_csv(f,col_types = cols(.default = col_character())) %>%
-      mutate(Session = as.character(Session),
-             UniqueName = as.character(UniqueName),
-             Run = as.character(Run))
-    if (clean){
-      tmp <- tmp %>% select(-starts_with('order-'),-starts_with('checkpoint-'),-starts_with('branch-'),-matches('Schedule ID'),-starts_with('Participant'),`Participant Private ID`)
-    }
-    d <- bind_rows(d,tmp)
-  }
-  d %>% select(Session,UniqueName,Run,matches('PID'),everything()) %>%
-    mutate(`UTC Date` = lubridate::dmy_hms(`UTC Date`),
-           `Local Date` = lubridate::dmy_hms(`Local Date`))
-}
+source('gimmedata.R')
 
 UpdateTables <- function(datadir = '/home/maximilien.chaumon/ownCloud/Lab/00-Projects/TimeSocialDistancing/DATA', wherefrom='online') {
   
