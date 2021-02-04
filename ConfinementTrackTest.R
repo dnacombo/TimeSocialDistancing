acce9dd41af417613432abc0d7d617ef5f7306b7
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2021-02-04 17:21:13 +0100

    add ConfinementTrackTest.R

---
+++ ConfinementTrackTest.R
@@ -0,0 +1,36 @@
+source('helpers.R')
+
+library(lubridate)
+
+datadir = '/home/maximilien.chaumon/ownCloud/Lab/00-Projects/TimeSocialDistancing/DATA'
+TimeTranslate <- gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1jLOOtz1WV2fgbJDkD26V0TZ3Yq6u9FVOYPq54PvGre4/edit#gid=0') 
+# ci-dessous pour intégrer une colonne "Country" dans la table data
+l <- paramsMatch(experimentIDs = ExperimentIDs)
+names(l$Country) <- l$ExperimentID
+
+data <- gimmedata(DataDir = datadir,UniqueName = "ConfinementTrack") %>%
+  mutate(Country = recode(`Experiment ID`,!!!l$Country),
+         .before = Session)
+
+if (exists('dataprocess')) rm("dataprocess")
+
+(dataprocess <- data %>% 
+    filter(Country == 'CA') %>%
+    group_by(Country) %>%
+    filter(!`Question Key`%in%c("BEGIN QUESTIONNAIRE","END QUESTIONNAIRE")) %>%
+    pivot_wider(id_cols = c(Country,Session,UniqueName,Run,`Participant Private ID`),names_from=`Question Key`,values_from=Response) %>%
+    mutate( detectsemaine = str_detect(confinement_tracker,filter(TimeTranslate, Country == Country[1]) %>% .$week),
+            detectmois = str_detect(confinement_tracker,filter(TimeTranslate, Country == Country[1]) %>% .$month),
+            confinement_tracker = ifelse( detectsemaine, parse_number(confinement_tracker) * 7, confinement_tracker),
+            confinement_tracker = ifelse( detectmois, parse_number(confinement_tracker) * 30, confinement_tracker),
+            ct1 = parse_date_time(confinement_tracker, orders = '%j'),
+            ct2 = as.duration(ct1 - as.POSIXct(dmy('01/01/2021'))),
+            confinement_tracker = as.numeric(ct2,'days')+1,
+           .after=confinement_tracker)) %>%
+  select(-ct1,-ct2,-starts_with('detect'))
+
+
+
+
+ggplot(dataprocess,aes(x=confinement_tracker,fill=Country)) +
+  geom_histogram(position=position_dodge(.5))
