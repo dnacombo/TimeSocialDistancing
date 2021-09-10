6d7fd243056502f200fdb2544a75b595a6fcc328
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2021-09-10 13:43:23 +0200

    update

--- daily_login_times.R
+++ daily_login_times.R
@@ -1,4 +1,4 @@
-datadir <- '/home/maximilien.chaumon/ownCloud/Lab/00-Projects/TimeSocialDistancing/DATA/RData'
+datadir <- '/home/maximilien.chaumon_local/ownCloud/Lab/00-Projects/TimeSocialDistancing/TSDshiny/data'
 
 source('helpers.R')
 
