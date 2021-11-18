aa48b7902b1b6d6276599bc2d272881f3e6b8ccb
Maximilien Chaumon <maximilien.chaumon@gmail.com>
2021-11-18 10:47:57 +0100

    Fix Subjective Confinement Indices to add Response to OSRN 16 to index

--- SubjectiveConfinementIndices.R
+++ SubjectiveConfinementIndices.R
@@ -5,7 +5,7 @@ SubjectiveConfinementIndices <-  gimmeRdata(dirBlursday, UniqueName = 'UCLA') %>
   filter(!`Question_Key`%in%c("BEGIN QUESTIONNAIRE","END QUESTIONNAIRE")) %>%
   pivot_wider(id_cols = c(Country, Session, Run, PID),names_from=`Question_Key`,values_from=Response, values_fn = first) %>%
   mutate(Reported_Loneliness = as.numeric(`OSRN-2-quantised`) + as.numeric(`OSRN-4-quantised`),
-         Felt_Loneliness = as.numeric(`OSRN-11-quantised`) + as.numeric(`OSRN-14-quantised`),
+         Felt_Loneliness = as.numeric(`OSRN-11-quantised`) + as.numeric(`OSRN-14-quantised`) + as.numeric(`OSRN-16-quantised`),
          Subjective_Confinement = Reported_Loneliness + Felt_Loneliness) %>%
   select(Country, Session, PID, Reported_Loneliness, Felt_Loneliness, Subjective_Confinement)
 
